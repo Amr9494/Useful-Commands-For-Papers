6b0acb55fb50f81cc567773e2dba119ceb1be94d
Amr9494 <95880703+Amr9494@users.noreply.github.com>
2025-12-30 14:51:54 +0200

    Update ieeefig.m to be applied locally on the current figure

--- ieeefig.m
+++ ieeefig.m
@@ -1,66 +1,61 @@
 function [f,fig] = ieeefig(ratio)
-%% IEEE Standard Figure Configuration - Version 1.0
+%% IEEE Standard Figure Configuration - Version 1.3
+% - Local scope (affects only this figure)
+% - Manual margin control to fix xlabel clipping
 
-% run this code before the plot command
-
-%%
-% According to the standard of IEEE Transactions and Journals: 
-
-% Times New Roman is the suggested font in labels. 
-
-% For a singlepart figure, labels should be in 8 to 10 points,
-% whereas for a multipart figure, labels should be in 8 points.
-
-% Width: column width: 8.8 cm; page width: 18.1 cm.
-
-%% width & hight of the figure
-fig.k_scaling = 3;          % scaling factor of the figure
-% (You need to plot a figure which has a width of (8.8 * k_scaling)
-% in MATLAB, so that when you paste it into your paper, the width will be
-% scalled down to 8.8 cm  which can guarantee a preferred clearness.
-fig.ratio = ratio;      % width:hight ratio of the figure
-
+%% 1. Figure Setup (Width & Height)
+fig.k_scaling = 3;          
+fig.ratio = ratio;      
 fig.width = 8.89 * fig.k_scaling;
 fig.hight = fig.width / fig.ratio;
 fig.fontsiz = 8; 
 fig.Fsiz = fig.fontsiz*fig.k_scaling;
-%% figure margins
-fig.top = 0.5;  % normalized top margin
-fig.bottom = 3;	% normalized bottom margin
-fig.left = 3.5;	% normalized left margin
-fig.right = 1;  % normalized right margin
 
-%% set default figure configurations
-set(0,'defaultFigureUnits','centimeters');
-set(0,'defaultFigurePosition',[0 0 fig.width fig.hight]);
+%% 2. Figure Margins (The section you requested)
+% These are in "units" (centimeters) relative to the scaled figure size.
+% We increased bottom/left slightly to make sure labels appear.
+fig.top = 0.5;      
+fig.bottom = 4.2;   % Space for X-Label (was 3)
+fig.left = 4.2;     % Space for Y-Label (was 3.5)
+fig.right = 1;      
 
-set(0,'defaultLineLineWidth',1*fig.k_scaling);
-set(0,'defaultAxesLineWidth',0.25*fig.k_scaling);
+%% 3. Create the Figure Object
+% We must create 'f' before we can apply the margin settings to it.
+f = figure('Units', 'centimeters', ...
+           'Position', [0 0 fig.width fig.hight], ...
+           'PaperPositionMode', 'auto');
 
-% set(0,'defaultAxesGridLineStyle',':');
-% set(0,'defaultAxesYGrid','on');
-% set(0,'defaultAxesXGrid','on');
+%% 4. Apply Margins to Axes Position
+% This converts your centimeter margins into Normalized units (0 to 1)
+% which MATLAB uses to place the inner axes box.
+% Format: [left bottom width height]
 
-set(0,'defaultAxesFontName','Times New Roman');
-set(0,'defaultAxesFontSize',fig.fontsiz*fig.k_scaling);
+axes_pos = [ ...
+    fig.left / fig.width, ...                           % Left
+    fig.bottom / fig.hight, ...                         % Bottom
+    (fig.width - fig.left - fig.right) / fig.width, ... % Width
+    (fig.hight - fig.bottom - fig.top) / fig.hight      % Height
+];
 
-set(0,'defaultTextFontName','Times New Roman');
-set(0,'defaultTextFontSize',fig.fontsiz*fig.k_scaling);
+% Apply ONLY to this figure (f), not globally (0)
+set(f, 'DefaultAxesPosition', axes_pos);
 
-set(0,'defaultLegendFontName','Times New Roman');
-set(0,'defaultLegendFontSize',fig.fontsiz*fig.k_scaling);
+%% 5. Other Visual Settings (Local)
+set(f, 'DefaultLineLineWidth', 1*fig.k_scaling);
+set(f, 'DefaultAxesLineWidth', 0.25*fig.k_scaling);
+set(f, 'DefaultAxesFontName', 'Times New Roman');
+set(f, 'DefaultAxesFontSize', fig.fontsiz*fig.k_scaling);
+set(f, 'DefaultAxesUnits', 'normalized');
+set(f, 'DefaultAxesColorOrder', get(0, 'factoryAxesColorOrder'));
+set(f, 'DefaultAxesTickDir', 'out');
 
-set(0,'defaultAxesUnits','normalized');
-set(0,'defaultAxesPosition',[fig.left/fig.width fig.bottom/fig.hight (fig.width-fig.left-fig.right)/fig.width  (fig.hight-fig.bottom-fig.top)/fig.hight]);
+% Text & Legend
+set(f, 'DefaultTextFontName', 'Times New Roman');
+set(f, 'DefaultTextFontSize', fig.fontsiz*fig.k_scaling);
+set(f, 'DefaultLegendFontName', 'Times New Roman');
+set(f, 'DefaultLegendFontSize', fig.fontsiz*fig.k_scaling);
+set(f, 'DefaultLegendLocation', 'southeast');
+set(f, 'DefaultLegendBox', 'on');
+set(f, 'DefaultLegendOrientation', 'vertical');
 
-set(0, 'defaultAxesColorOrder', get(0, 'factoryAxesColorOrder'));
-set(0,'defaultAxesTickDir','out');
-
-set(0,'defaultFigurePaperPositionMode','auto');
-
-% you can change the Legend Location to whatever as you wish
-set(0,'defaultLegendLocation','southeast');
-set(0,'defaultLegendBox','on');
-set(0,'defaultLegendOrientation','vertical');
-figure
-f = gcf; 
+end
